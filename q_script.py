8d0cbe9e7ffe4e0bb9ba57810f5068625574d0f0
TropicalNerd <praenubilus@gmail.com>
2017-08-15 00:36:46 -0400

    add tag and id options

--- q_script.py
+++ q_script.py
@@ -4,12 +4,17 @@ import shutil
 
 parser = ap.ArgumentParser(description='Auto grader script.')
 parser.add_argument('-n', '--name', required=True, dest='problem_name', default=[],
-                    help="the downloaded submission package from canvas")
-
+                    help="the name of question")
+parser.add_argument('-i', '--id', dest='problem_id',
+                    help="the id number of question on leetcode")
+parser.add_argument('-t', '--tag', dest='problem_tags', nargs='+',
+                    help="the tags for the problem")
 args = parser.parse_args()
 
 if __name__ == '__main__':
     problem_name = args.problem_name
+    problem_id = args.problem_id
+    problem_tags = args.problem_tags
     os.makedirs(problem_name)
     readme_file_path = os.path.join(problem_name, 'README.md')
     shutil.copy('q_template.md', readme_file_path)
@@ -19,7 +24,20 @@ if __name__ == '__main__':
         data = file.readlines()
 
     # edit File Title to problem name
-    data[2] = '# ' + problem_name + '\n'
+    if problem_tags is not None:
+        data[3] = 'Tags:'
+        for tag in problem_tags:
+            data[3] += ' __' + tag + '__,'
+
+        data[3] = data[3][:-1]
+
+    if problem_id is not None:
+        data[2] = '# ' + problem_id + '. ' + problem_name + '\n'
+        data[10] = data[10].replace('qxxx', ('' + problem_id).zfill(3))
+        del data[9]
+        del data[8]
+    else:
+        data[2] = '# ' + problem_name + '\n'
 
     # and write everything back
     with open(readme_file_path, 'w') as file:
